56dad0239e7271c13bd7dc654ffcc24fc136320d
CBreakr <github email address>
2020-06-24 16:13:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function findMinAndRemove(array){
+    let min = Number.POSITIVE_INFINITY;
+    let min_index = 0;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] < min){
+            min = array[i];
+            min_index = i;
+        }
+    }
 
+    array.splice(min_index, 1);
+
+    return min;
 }
 
 function insertionSort(array){
+    const sorted = [];
+    const length = array.length;
 
+    for(let i = 0; i < length; i++){
+        let min = findMinAndRemove(array);
+        console.log(min);
+        sorted.push(min);
+    }
+
+    return sorted;
 }
