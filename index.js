3e06f2a642af9547c3f7e46fbac9f1a0ace17523
MisterC1ean <robert.keller44@icloud.com>
2020-06-25 15:16:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let smallest = array[0];
+  let smIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < smallest) {
+      smallest = array[i];
+      smIndex = i;
+    }
+  }
+  array.splice(smIndex, 1);
+  return smallest;
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let sorted = [];
+  let smallest;
+  while (array.length != 0) {
+    smallest = findMinAndRemove(array)
+    sorted.push(smallest)
+  }
+  return sorted;
 }
