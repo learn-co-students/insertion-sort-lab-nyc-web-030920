97e3b6ad270452c5eedf928ab87c72550b0a43e5
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-07-01 13:04:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+// √ min equal to first element in array
+// √ loop through the array
+// √ compare each element to min
+// √ if its less than min, element becomes new min,
+// √ remove min from array
+// √ return min
+
+
 function findMinAndRemove(array){
     let min = array[0];
-    let minIndex = 0;
-    for (let i = 0; i < array.length; i++){
-        if(array[i] < min) {
+    let minIndex = 0
+    for(let i = 0; i < array.length; i++){
+        if(array[i] < min){
             min = array[i]
             minIndex = i
         }
@@ -11,6 +19,19 @@ function findMinAndRemove(array){
     return min
 }
 
-function insertionSort(array){
+// declare variable sorted = empty array
+// declare variable min
+// loop through the array
+// set variable min === lowest number returned from findMinAndRemove()
+// push min into sorted array
+// return the sorted array
 
+function insertionSort(array){
+    let sorted = [];
+    let min;
+    while (array.length !== 0){
+        min = findMinAndRemove(array)
+        sorted.push(min)
+    }
+    return sorted
 }
