0c8b2b7acb71f5720c1f9afba8de08cffb0b0d94
JohnsonNYC <jkow95@gmail.com>
2020-07-02 13:33:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
+    let min = array[0]; // pick first number
+    let minIndex = 0; // pick position of that first number 
 
+    for(let i = 0; i < array.length; i++){ // iterate through each 
+        if(array[i] < min){ // if array element is less than "min"
+            min = array[i]; // set min too array element
+            minIndex =  i; // set position to array index
+        }
+    }
+    array.splice(minIndex,1);  // starting at minIndex, remove 1 element
+    return min; 
 }
 
 function insertionSort(array){
-
+    // run minAndRemove multiple times until array is empty 
+    // take the return value and append it to a new array that will be sorted 
+    let sorted = []
+    while(array.length > 0){
+        let value = findMinAndRemove(array)
+        sorted.push(value)
+    }
+    return sorted
 }
