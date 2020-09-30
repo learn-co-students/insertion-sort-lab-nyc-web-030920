a2103e140470db5523a79cdfd83bc4f31fc1471c
Leon Jamison <leonjamison@icloud.com>
2020-09-30 16:41:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function findMinAndRemove(array){
 
-}
+    let min = Math.min(...array)
+    array.splice(array.indexOf(min), 1)
+    return min
+
+} 
+
 
 function selectionSort(array){
+    let sortedArray = []
+    let min;
+    while (array.length != 0){
+     let min = findMinAndRemove(array)
+       sortedArray.push(min)
+    }return sortedArray
 
 }
